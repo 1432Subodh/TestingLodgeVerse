d854fa33a144de68574412bab0f8e6a71832c1c9
Subodh Ravidas <subodh14329@gmail.com>
2024-09-24 10:03:12 +0530

    'update'

--- src/components/component-ui/navbar/Searchbar.tsx
+++ src/components/component-ui/navbar/Searchbar.tsx
@@ -23,7 +23,7 @@ function Searchbar() {
             <MagnifyingGlassIcon width="22" height="22" className='sm:hidden' />
           </div>
 
-          <div className='capitalize sm:flex  gap-2 items-center hidden transition-all text-gray-300 hover:bg-zinc-900 hover:text-accent-foreground text-xs font-semibold cursor-pointer border rounded-[.5rem] px-3 py-1.5 w-64 '>
+          <div className='capitalize sm:flex  gap-2 items-center hidden transition-all text-gray-300 hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:text-accent-foreground text-xs font-semibold cursor-pointer border rounded-[.5rem] px-3 py-1.5 w-64 '>
             <MagnifyingGlassIcon width="20" height="20" />
             Search for stay...
           </div>
