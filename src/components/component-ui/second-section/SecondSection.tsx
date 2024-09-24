85aca8df154b07e20a690c845e0118e6a6aa2cf5
Subodh Ravidas <subodh14329@gmail.com>
2024-09-24 09:27:13 +0530

    'intial'

---
+++ src/components/component-ui/second-section/SecondSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SecondSection() {
+    return (
+        <div className='mt-[10vh]'>
+            <h2 className="scroll-m-20 text-2xl px-3 font-bold tracking-tight lg:text-2xl text-center">About lodge/ui</h2>
+
+        </div>
+    )
+}
+
+export default SecondSection
