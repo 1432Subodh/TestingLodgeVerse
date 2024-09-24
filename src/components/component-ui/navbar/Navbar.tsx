f9a8ba39deadd5d24fa4ca50339e081382c236f5
Subodh Ravidas <subodh14329@gmail.com>
2024-09-24 09:46:42 +0530

    'intial'

--- src/components/component-ui/navbar/Navbar.tsx
+++ src/components/component-ui/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import { Navigation } from './Navigation'
 import Searchbar from './Searchbar'
 import { AvatarDemo } from './Avatar'
 import { SheetDemo } from './SideMenu'
+import Image from 'next/image'
 
 function Navbar() {
   return (
@@ -11,7 +12,7 @@ function Navbar() {
       <div className='flex items-center gap-10'>
 
         <div className='flex gap-2 items-center'>
-          <img src="svg/logo.svg" className='dark:invert w-6' alt="" />
+          <Image width='24' height={10} src="svg/logo.svg" className='dark:invert w-6' alt="" />
           <h1 className='font-bold text-normal '>lodge/ui</h1>
         </div>
         <Navigation />
