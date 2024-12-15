03d4b7391f51a2567103ede0d47c43cce37f3acb
Subodh Ravidas <subodh14329@gmail.com>
2024-12-15 23:32:01 +0530

    adf

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,51 +1,10 @@
-'use client'
-import { Dashboard } from '@/components/route/admin/Dashboard'
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React from 'react'
+import AdminPage from './AdminPage'
 
 function page() {
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-            // Safely access localStorage inside useEffect
-            const userString = localStorage.getItem('user');
-            // console.log(userString)
-            if (userString) {
-                const userData = JSON.parse(userString);
-                if(userData.uid==='IviFmSYe3hWqsDsM8EpASJbJQhV2'){
-
-                    setIsLoggedIn(true); // Update logged-in state
-                }
-                console.log(userData);
-            } else {
-                console.log('No user found in localStorage.');
-            }
-        }, [])
-
-    return (
-        <>
-            <div className='w-full'>
-                {
-                    isLoggedIn ? 
-                    <Dashboard />:
-                    <div className='w-full h-[90vh] flex justify-center items-center'>
-                        <div>
-                            <p>Login with Admin</p>
-                            <div>
-                                <Link href={'/ClientAuth/signin?admin=true'}>
-                                <Button variant={'outline'} className='border'>Login</Button>
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                }
-
-            </div>
-        </>
-    )
+  return (
+    <AdminPage/>
+  )
 }
 
 export default page
